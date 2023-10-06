7c841fd2f4498be1045468a1ce25d0ba34e40a13
lisal00 <143776518+lisal00@users.noreply.github.com>
2023-10-05 23:16:41 -0500

    Update Single_Linked_List.cpp

--- Question1/Single_Linked_List.cpp
+++ Question1/Single_Linked_List.cpp
@@ -1,6 +1,6 @@
 #include "Single_Linked_List.h"
 #include <stddef.h>
-
+#include <stdexcept>
 using namespace std;
 
 /**Constructor*/
@@ -11,24 +11,11 @@ Single_Linked_List<Item_Type>::Single_Linked_List(){
     num_items = 0;
 }
 
-/** Construct a copy of a list. 
-template<typename Item_Type>
-Single_Linked_List<Item_Type>::Single_Linked_List(const Single_Linked_List<Item_Type>& other) {
-    head = NULL;
-    tail = NULL;
-    num_items = 0;
-    for (const_iterator itr = other.begin();
-        itr != other.end(); ++itr) {
-        push_back(*itr);
-    }
-}
-*/
-
 /** Destructor*/
 template<typename Item_Type>
 Single_Linked_List<Item_Type>::~Single_Linked_List() {
     while (head != NULL) {
-        Node* current = head;
+        Node<Item_Type>* current = head;
         head = head->next;
         delete current;
     }
@@ -66,9 +53,9 @@ void Single_Linked_List<Item_Type>::push_back(const Item_Type& item){
 template<typename Item_Type>
 void Single_Linked_List<Item_Type>::pop_front(){
     if (head == NULL){ //checks if list is empty
-        throw invalid_argument("List is empty.");
+        throw invalid_argument ("List is empty.");
     }
-    Node* ptr = head;
+    Node<Item_Type>* ptr = head;
     head = head->next;
     delete ptr;
     if (head == NULL){ //checks again if head is null aka empty list
@@ -81,20 +68,20 @@ void Single_Linked_List<Item_Type>::pop_front(){
 template<typename Item_Type>
 void Single_Linked_List<Item_Type>::pop_back(){
     if (tail == NULL){ //checks if list is empty
-        throw invalid_argument("List is empty.");
+        throw invalid_argument ("List is empty.");
     }
     if (head == tail){//checks if only 1 element in list
         delete head;
         head = NULL;
         tail = NULL;
     } else{ //2+ elements in list
-        Node* current = head;
+        Node<Item_Type>* current = head;
         while (current->next != tail){//goes thru list to determine what tail will be after pop_back()
             current = current->next;
         }
-    delete tail;
-    tail = current;
-    tail->next = NULL;
+        delete tail;
+        tail = current;
+        tail->next = NULL;
     }
     num_items--;
 }
@@ -107,7 +94,7 @@ Item_Type Single_Linked_List<Item_Type>::front(){
     if (head != NULL){
         return head->data;
     } else{
-        throw("List is empty.");
+        throw invalid_argument("List is empty.");
     }
 }
 
@@ -119,7 +106,7 @@ Item_Type Single_Linked_List<Item_Type>::back(){
     if (tail != NULL){
         return tail->data;
     } else{
-        throw("List is empty.");
+        throw invalid_argument("List is empty.");
     }
 }
 
@@ -128,7 +115,7 @@ Item_Type Single_Linked_List<Item_Type>::back(){
 */
 template<typename Item_Type>
 bool Single_Linked_List<Item_Type>::empty(){
-    if (head == NULL && tail == NULL){
+    if (head == NULL && tail == NULL && num_items == 0){
         return true; //is empty
     } else {
         return false; //is not empty
@@ -142,7 +129,22 @@ bool Single_Linked_List<Item_Type>::empty(){
 */
 template<typename Item_Type>
 void Single_Linked_List<Item_Type>::insert(size_t index, const Item_Type& item){
-
+    Node<Item_Type>* item_node = new Node(item); //create item node
+    if (index == 0 || head == NULL){ //index is 0 or list is empty
+        push_front(item);
+        return;
+    }
+    if (index >= num_items){//index is out of bounds
+        push_back(item);
+    } else {//regular case
+        Node<Item_Type>* current = head;
+        for (int i = 0; i < index-1; i++){
+            current = current->next;
+        }
+        item_node->next = current->next;
+        current->next = item_node;
+        num_items++;
+    }
 }
 
 /** Remove the item at position index. 
@@ -152,7 +154,28 @@ void Single_Linked_List<Item_Type>::insert(size_t index, const Item_Type& item){
 */
 template<typename Item_Type>
 bool Single_Linked_List<Item_Type>::remove(size_t index){
+    if (index == 0){
+        pop_front();
+        return true;
+    }
+    if (index == (num_items-1)){ //if index is end of list
+        pop_back();
+        return true;
+    }
+    if (head != NULL && index < num_items){//checks if list is not empty and index in bounds
+        Node<Item_Type>* current = head;
+        for (int i = 0; i < index-1; i++){
+            current = current->next;
+        }
+        Node<Item_Type>* delete_node = current->next;
+        current->next = delete_node->next;
+        delete delete_node;
+        num_items--;
 
+        return true;
+    } else{
+        return false;
+    }
 }
 
 /** Searches for item in list
@@ -162,5 +185,14 @@ bool Single_Linked_List<Item_Type>::remove(size_t index){
 */
 template<typename Item_Type>
 size_t Single_Linked_List<Item_Type>::find(const Item_Type& item){
-
+    Node<Item_Type>* current = head;
+    size_t index = 0;
+    while (current != NULL){
+        if (item == current->data){
+            return index;
+        }
+        current = current->next;
+        index++;
+    }
+    return num_items;
 }
